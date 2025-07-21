4fdac31e9f227b1d48c15847478b5730a6488997
Sush1sui <jplovesraw@gmail.com>
2025-07-21 20:59:26 +0800

    refactor: better ping logging

--- internal/bot/helpers/pingServer.go
+++ internal/bot/helpers/pingServer.go
@@ -10,9 +10,6 @@ import (
 func PingServerLoop(serverURL string) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for {
-		minutes := r.Intn(5) + 10 // 10-14 inclusive
-		fmt.Printf("Waiting %d minutes before pinging server...\n", minutes)
-		time.Sleep(time.Duration(minutes) * time.Minute)
 		resp, err := http.Get(serverURL)
 		if err != nil {
 			fmt.Printf("Ping failed: %v\n", err)
@@ -23,6 +20,9 @@ func PingServerLoop(serverURL string) {
 			fmt.Printf("Server returned non-200 status code: %d\n", resp.StatusCode)
 			continue
 		}
-		fmt.Printf("Server is reachable, Status:%s\n", resp.Status)
+		fmt.Printf("Server is reachable, Status: %s\n", resp.Status)
+		minutes := r.Intn(5) + 10 // 10-14 inclusive
+		fmt.Printf("Waiting %d minutes before pinging server...\n", minutes)
+		time.Sleep(time.Duration(minutes) * time.Minute)
 	}
 }
